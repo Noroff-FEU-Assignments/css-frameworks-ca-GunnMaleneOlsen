1d1866be50426a9929a101f0e6e70d9a039676b3
Gunn Malene <gm.plazaolsen@gmail.com>
2021-09-14 19:28:18 +0200

    finnished putting components on News page

---
+++ ca1/src/components/news/Cards.js
@@ -0,0 +1,100 @@
+import React from "react";
+import { Button, Card } from "react-bootstrap";
+
+function Cards() {
+  return (
+    <div className="news__cards">
+      <Card style={{ width: "18rem" }}>
+        <Card.Img variant="top" src="News images/news-1.jpg" />
+        <Card.Body>
+          <Card.Title>Nunc porttitor vel</Card.Title>
+          <Card.Text>Nunc malesuada eget est fringilla dapibus.</Card.Text>
+          <Button variant="primary">
+              More
+              <div className="btn-primary--syle-animation"></div>
+            </Button>
+        </Card.Body>
+      </Card>
+      <Card style={{ width: "18rem" }}>
+        <Card.Img variant="top" src="News images/news-1.jpg" />
+        <Card.Body>
+          <Card.Title>Nunc porttitor vel</Card.Title>
+          <Card.Text>Nunc malesuada eget est fringilla dapibus.</Card.Text>
+          <Button variant="primary">
+              More
+              <div className="btn-primary--syle-animation"></div>
+            </Button>
+        </Card.Body>
+      </Card>
+      <Card style={{ width: "18rem" }}>
+        <Card.Img variant="top" src="News images/news-1.jpg" />
+        <Card.Body>
+          <Card.Title>Nunc porttitor vel</Card.Title>
+          <Card.Text>Nunc malesuada eget est fringilla dapibus.</Card.Text>
+          <Button variant="primary">
+              More
+              <div className="btn-primary--syle-animation"></div>
+            </Button>
+        </Card.Body>
+      </Card>
+      <Card style={{ width: "18rem" }}>
+        <Card.Img variant="top" src="News images/news-1.jpg" />
+        <Card.Body>
+          <Card.Title>Nunc porttitor vel</Card.Title>
+          <Card.Text>Nunc malesuada eget est fringilla dapibus.</Card.Text>
+          <Button variant="primary">
+              More
+              <div className="btn-primary--syle-animation"></div>
+            </Button>
+        </Card.Body>
+      </Card>
+      <Card style={{ width: "18rem" }}>
+        <Card.Img variant="top" src="News images/news-1.jpg" />
+        <Card.Body>
+          <Card.Title>Nunc porttitor vel</Card.Title>
+          <Card.Text>Nunc malesuada eget est fringilla dapibus.</Card.Text>
+          <Button variant="primary">
+              More
+              <div className="btn-primary--syle-animation"></div>
+            </Button>
+        </Card.Body>
+      </Card>
+      <Card style={{ width: "18rem" }}>
+        <Card.Img variant="top" src="News images/news-1.jpg" />
+        <Card.Body>
+          <Card.Title>Nunc porttitor vel</Card.Title>
+          <Card.Text>Nunc malesuada eget est fringilla dapibus.</Card.Text>
+          <Button variant="primary">
+              More
+              <div className="btn-primary--syle-animation"></div>
+            </Button>
+        </Card.Body>
+      </Card>
+      <Card style={{ width: "18rem" }}>
+        <Card.Img variant="top" src="News images/news-1.jpg" />
+        <Card.Body>
+          <Card.Title>Nunc porttitor vel</Card.Title>
+          <Card.Text>Nunc malesuada eget est fringilla dapibus.</Card.Text>
+          <Button variant="primary">
+              More
+              <div className="btn-primary--syle-animation"></div>
+            </Button>
+        </Card.Body>
+      </Card>
+      <Card style={{ width: "18rem" }}>
+        <Card.Img variant="top" src="News images/news-1.jpg" />
+        <Card.Body>
+          <Card.Title>Nunc porttitor vel</Card.Title>
+          <Card.Text>Nunc malesuada eget est fringilla dapibus.</Card.Text>
+          <Button variant="primary">
+              More
+              <div className="btn-primary--syle-animation"></div>
+            </Button>
+        </Card.Body>
+      </Card>
+
+    </div>
+  );
+}
+
+export default Cards;
--- ca1/src/components/news/News.js
+++ ca1/src/components/news/News.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
+import Cards from "./Cards";
+import Paginations from "./Paginations";
 
 function News() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <div className="news">
+      <h1>News</h1>
+      <Paginations />
+      <Cards />
+      <Paginations />
+    </div>
+  );
 }
 
-export default News
+export default News;
