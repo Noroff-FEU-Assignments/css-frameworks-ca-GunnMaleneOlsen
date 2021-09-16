337e6c0f73066d9badb6364fb9e244371ddf87f2
Gunn Malene <gm.plazaolsen@gmail.com>
2021-09-16 11:16:31 +0200

    some layout and basic styling

--- ca1/src/App.js
+++ ca1/src/App.js
@@ -17,58 +17,55 @@ import Contact from "./components/contact/Contact";
 function App() {
   return (
     <Router>
-      <div>
-        <div className="wrapper">
-          <Navbar bg="light" expand="lg">
-            <Navbar.Brand href="/">The YAY company</Navbar.Brand>
-            <Navbar.Toggle aria-controls="navbarScroll" />
-            <Navbar.Collapse id="navbarScroll">
-              <Nav>
-                <NavLink to="/" className="nav-link">
-                  Home
-                </NavLink>
-                <NavLink to="/news" className="nav-link">
-                  News
-                </NavLink>
-                <NavLink to="/contact" className="nav-link">
-                  Contact
-                </NavLink>
-              </Nav>
-              <Form className="d-flex">
-                <FormControl
-                  type="search"
-                  placeholder="Search"
-                  className="mr-2"
-                  aria-label="Search"
-                />
-                <Button variant="outline-success">Go</Button>
-              </Form>
-            </Navbar.Collapse>
-          </Navbar>
+      <div className="wrapper">
+        <Navbar bg="light" expand="lg">
+          <Navbar.Brand href="/">The YAY company</Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav>
+              <NavLink to="/" className="nav-link">
+                Home
+              </NavLink>
+              <NavLink to="/news" className="nav-link">
+                News
+              </NavLink>
+              <NavLink to="/contact" className="nav-link">
+                Contact
+              </NavLink>
+            </Nav>
+            <Form className="d-flex">
+              <FormControl
+                type="search"
+                placeholder="Search"
+                className="mr-2"
+                aria-label="Search"
+              />
+              <Button variant="outline-success">Go</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Navbar>
 
-          <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route path="/news">
-              <News />
-            </Route>
-            <Route path="/contact">
-              <Contact />
-            </Route>
-          </Switch>
-        </div>
-        
-        <footer>
-          <div className="footer__socialLinks">
-            <FontAwesomeIcon icon={faVimeoV} />
-            <FontAwesomeIcon icon={faYoutube} />
-          </div>
-          <span className="footer__info">hello@yay.com</span>
-          <span className="footer__info">Copyright 2020</span>
-        </footer>
-
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/news">
+            <News />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+        </Switch>
       </div>
+
+      <footer>
+        <div className="footer__socialLinks">
+          <FontAwesomeIcon icon={faVimeoV} />
+          <FontAwesomeIcon icon={faYoutube} />
+        </div>
+        <span className="footer__info">hello@yay.com</span>
+        <span className="footer__info">Copyright 2020</span>
+      </footer>
     </Router>
   );
 }
