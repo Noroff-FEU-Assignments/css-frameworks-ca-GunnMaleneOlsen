af4eaf7db24f9ae8fda1262051ef7b50be3d2b81
Gunn Malene <gm.plazaolsen@gmail.com>
2021-09-16 19:12:23 +0200

    finished style tabs

--- ca1/src/components/home/TabsComponent.js
+++ ca1/src/components/home/TabsComponent.js
@@ -5,83 +5,61 @@ import { Container, Tab, Tabs } from "react-bootstrap";
 
 function TabsComponent() {
   return (
-    <Container className="d-none d-md-block">
-      <Tabs
-        defaultActiveKey="first"
-        id="uncontrolled-tab-example"
-        className="mb-3 "
-      >
+    <Container className="d-none d-md-block tab-container">
+      <Tabs defaultActiveKey="first" id="uncontrolled-tab-example" className="tabs ">
         <Tab className="tab-content__page" eventKey="first" title="First">
+        <img className="d-block tab-content__image" src="Tab images/tab-1.jpg" alt="First Tab" />
           <div className="tab-content__wrapper">
             <p className="tab-content__paragraph">
-              Morbi eget efficitur turpis. Vivamus pellentesque tortor massa,
-              venenatis pharetra leo laoreet a. Nullam non eleifend justo, a
-              ullamcorper turpis. Cras vehicula pharetra lectus non maximus. Sed
-              condimentum mattis rhoncus.
+              Morbi eget efficitur turpis. Vivamus pellentesque tortor massa, venenatis pharetra leo laoreet a. Nullam non eleifend justo, a
+              ullamcorper turpis. Cras vehicula pharetra lectus non maximus. Sed condimentum mattis rhoncus.
             </p>
-            <div className="tab__socials">
-              <span>Share</span>
-              <a className="tab__link" href="/">
+            <div className="tab-content__socials">
+              <span>SHARE</span>
+              <a className="tab-content__link" href="/">
                 <FontAwesomeIcon icon={faFacebookF} />
               </a>
-              <a className="tab__link" href="/">
+              <a className="tab-content__link" href="/">
                 <FontAwesomeIcon icon={faTwitter} />
               </a>
             </div>
           </div>
-          <img
-            className="d-block tab-content__image"
-            src="Tab images/tab-1.jpg"
-            alt="First Tab"
-          />
         </Tab>
         <Tab className="tab-content__page" eventKey="second" title="Second">
+        <img className="d-block tab-content__image" src="Tab images/tab-2.jpg" alt="First Tab " />
           <div className="tab-content__wrapper">
             <p className="tab-content__paragraph">
-              Morbi eget efficitur turpis. Vivamus pellentesque tortor massa,
-              venenatis pharetra leo laoreet a. Nullam non eleifend justo, a
-              ullamcorper turpis. Cras vehicula pharetra lectus non maximus. Sed
-              condimentum mattis rhoncus.
+              Morbi eget efficitur turpis. Vivamus pellentesque tortor massa, venenatis pharetra leo laoreet a. Nullam non eleifend justo, a
+              ullamcorper turpis. Cras vehicula pharetra lectus non maximus. Sed condimentum mattis rhoncus.
             </p>
-            <div className="tab__socials">
-              <span>Share</span>
-              <a className="tab__link" href="/">
+            <div className="tab-content__socials">
+              <span>SHARE</span>
+              <a className="tab-content__link" href="/">
                 <FontAwesomeIcon icon={faFacebookF} />
               </a>
-              <a className="tab__link" href="/">
+              <a className="tab-content__link" href="/">
                 <FontAwesomeIcon icon={faTwitter} />
               </a>
             </div>
           </div>
-          <img
-            className="d-block tab-content__image"
-            src="Tab images/tab-2.jpg"
-            alt="First Tab "
-          />
         </Tab>
         <Tab className="tab-content__page" eventKey="third" title="Third">
+          <img className="d-block tab-content__image" src="Tab images/tab-3.jpg" alt="First Tab" />
           <div className="tab-content__wrapper">
             <p className="tab-content__paragraph">
-              Morbi eget efficitur turpis. Vivamus pellentesque tortor massa,
-              venenatis pharetra leo laoreet a. Nullam non eleifend justo, a
-              ullamcorper turpis. Cras vehicula pharetra lectus non maximus. Sed
-              condimentum mattis rhoncus.
+              Morbi eget efficitur turpis. Vivamus pellentesque tortor massa, venenatis pharetra leo laoreet a. Nullam non eleifend justo, a
+              ullamcorper turpis. Cras vehicula pharetra lectus non maximus. Sed condimentum mattis rhoncus.
             </p>
-            <div className="tab__socials">
-              <span>Share</span>
-              <a className="tab__link" href="/">
+            <div className="tab-content__socials">
+              <span>SHARE</span>
+              <a className="tab-content__link" href="/">
                 <FontAwesomeIcon icon={faFacebookF} />
               </a>
-              <a className="tab__link" href="/">
+              <a className="tab-content__link" href="/">
                 <FontAwesomeIcon icon={faTwitter} />
               </a>
             </div>
           </div>
-          <img
-            className="d-block tab-content__image"
-            src="Tab images/tab-3.jpg"
-            alt="First Tab"
-          />
         </Tab>
       </Tabs>
     </Container>
